a10d40fe6242b7ed0ddcd3a613d353ae8f8df37f
wzsayiie <wzsayiie@qq.com>
2022-09-03 23:51:38 +0800

    adjusted header inclusion.

--- srcutils/fileutil.c
+++ srcutils/fileutil.c
@@ -1,5 +1,4 @@
 #include "fileutil.h"
-#include "fileio.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
--- srcutils/fileutil.h
+++ srcutils/fileutil.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "stdbool.h"
-#include "stddef.h"
+#include "fileio.h"
 
 #if __cplusplus
 extern "C" {
