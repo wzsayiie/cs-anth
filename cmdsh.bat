397ab96eed888de68e516c87ed96f90b06bb021d
wzsayiie <wzsayiie@qq.com>
2022-09-10 17:18:41 +0800

    shell configuration.

--- cmdsh.bat
+++ cmdsh.bat
@@ -1,7 +1,28 @@
 @echo off
 
-setlocal
+::root directory.
+set ANTH_HOME=%~dp0
+if %ANTH_HOME:~-1%==\ (
+    ::remove backslash at the end.
+    set ANTH_HOME=%ANTH_HOME:~0,-1%
+)
 
-cd %~dp0
+::"path".
+set PATH=%PATH%;%ANTH_HOME%\bin
 
-endlocal
+::commands.
+doskey cat   = type $*
+doskey cd    = cd /d $1
+doskey clear = cls
+doskey ls    = dir /d $*
+doskey mv    = move $*
+doskey open  = start "" $*
+doskey which = where $*
+
+::shell configuration.
+prompt $p$s%%$s
+
+::user configutation.
+if exist %ANTH_HOME%\ucfg.bat (
+    call %ANTH_HOME%\ucfg.bat
+)
--- cmdsh.c
+++ cmdsh.c
@@ -1,4 +1,5 @@
-#include "fileop.h"
+#include "fileutils.h"
+#include "libgen.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -9,12 +10,15 @@
 int main() {
     //get the bat file.
     char path[BUF_SIZE];
-    getexed(path, BUF_SIZE);
+    getexepath(path, BUF_SIZE);
+
+    dirname(path);
     pathcat(path, BAT_FILE);
 
     //check if the bat file exists.
     if (!fexists(path, NULL)) {
         printf("not found bat file '%s'\n", BAT_FILE);
+        system("pause");
         return 1;
     }
 
