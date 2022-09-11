74191ac8177b6bfb78e3e7661ddda0df6a2833b3
wujia <wzsayiie@qq.com>
2022-09-11 15:58:20 +0800

    added directory operation functions.

--- alibh/fileio_win.c
+++ alibh/fileio_win.c
@@ -66,13 +66,50 @@ bool _h_denter(const char *path) {
     return !err;
 }
 
+struct _adir {
+    HANDLE           handle;
+    WIN32_FIND_DATAA dat   ;
+    bool             first ;
+};
+
 ADIR *_h_dopen(const char *path) {
-    return NULL;
+    size_t plen = strlen(path);
+    char  *name = malloc(plen + 3);
+    strcpy(name, path);
+    if (name[plen - 1] != '\\' && name[plen - 1] != '/') {
+        strcpy(name + plen, "\\*");
+    } else {
+        strcpy(name + plen, "*");
+    }
+
+    ADIR *dir   = calloc(1, sizeof(ADIR));
+    dir->handle = FindFirstFileA(name, &dir->dat);
+    dir->first  = true;
+
+    free(name);
+
+    if (dir->handle == INVALID_HANDLE_VALUE) {
+        free(dir);
+        return NULL;
+    }
+    return dir;
 }
 
 char *_h_dcopy(ADIR *dir) {
+    if (dir->first) {
+        dir->first = false;
+        return strdup(dir->dat.cFileName);
+    }
+
+    BOOL success = FindNextFileA(dir->handle, &dir->dat);
+    if (success) {
+        return strdup(dir->dat.cFileName);
+    }
+
     return NULL;
 }
 
 void _h_dclose(ADIR *dir) {
+    FindClose(dir->handle);
+    free(dir);
 }
