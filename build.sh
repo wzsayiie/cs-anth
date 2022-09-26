d873f75101259d735b6de6ab8f6e4a3545940879
wzsayiie <wzsayiie@qq.com>
2022-09-26 22:45:33 +0800

    adjusted scripts.

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 set -e -u
 
 cd `dirname $0`
--- rc.sh
+++ rc.sh
@@ -1,9 +1,9 @@
 # shell configuration.
-if [[ "$0" == "bash" ]]; then
+if [ "$0" = "bash" ]; then
     export ANTH_HOME=$(cd $(dirname $BASH_SOURCE) && pwd)
     export PS1="\e[32m\w \e[37m\$\e[0m "
 
-elif [[ "$0" == "ksh" ]]; then
+elif [ "$0" = "ksh" ]; then
     export ANTH_HOME=$(cd $(dirname ${.sh.file} ) && pwd)
     export PS1="\$PWD \$ "
 
@@ -13,14 +13,9 @@ else # zsh.
 fi
 
 # environment:
-export PATH=\
-$ANTH_HOME/bin:\
-$PATH
+export PATH=$ANTH_HOME/bin:$PATH
 
-export CPATH=\
-$ANTH_HOME/libextend:\
-$ANTH_HOME/libhost
-
+export             CPATH=$ANTH_HOME/libextend:$ANTH_HOME/libhost
 export      LIBRARY_PATH=$ANTH_HOME/lib
 export DYLD_LIBRARY_PATH=$ANTH_HOME/lib
 export   LD_LIBRARY_PATH=$ANTH_HOME/lib
@@ -36,6 +31,6 @@ export CLICOLOR=1
 export LS_COLORS="di=34"
 
 # user rc.
-if [[ -e $ANTH_HOME/urc.sh ]]; then
+if [ -e $ANTH_HOME/urc.sh ]; then
     source $ANTH_HOME/urc.sh
 fi
