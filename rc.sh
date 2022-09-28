f9b7e6079c75216fcd10c949e73ffc58ab4346b7
wujia <wzsayiie@qq.com>
2022-09-28 21:00:16 +0800

    added support for dash.

--- rc.sh
+++ rc.sh
@@ -1,11 +1,20 @@
 # shell configuration.
 if [ "$0" = "bash" ]; then
     export ANTH_HOME=$(cd $(dirname $BASH_SOURCE) && pwd)
-    export PS1="\e[32m\w \e[37m\$\e[0m "
+    export PS1="\033[32m\w \033[37m\$\033[0m "
+
+elif [ "$0" = "dash" ]; then
+    if [ "$ANTH_HOME" = "" ] || [ ! -e "$ANTH_HOME/rc.sh" ]; then
+        # can not get the directory of running script.
+        # need user to set ANTH_HOME manually.
+        echo "need to configure ANTH_HOME manually"
+        return
+    fi
+    export PS1="$(echo "\033[32m$PWD \033[37m\$\033[0m ")"
 
 elif [ "$0" = "ksh" ]; then
-    export ANTH_HOME=$(cd $(dirname ${.sh.file} ) && pwd)
-    export PS1="\$PWD \$ "
+    export ANTH_HOME=$(cd $(dirname ${.sh.file}) && pwd)
+    export PS1="$(echo "\033[32m$PWD \033[37m\$\033[0m ")"
 
 else # zsh.
     export ANTH_HOME=$0:a:h
@@ -24,11 +33,11 @@ export   LD_LIBRARY_PATH=$ANTH_HOME/lib
 alias cls="clear"
 
 # preferences:
+
+# mac ls color setting for 11 different file types.
 #..............=1.2.3.4.5.6.7.8.9.a.b.
 export LSCOLORS=exaxaxaxaxaxaxaxaxaxax
 export CLICOLOR=1
-
-export LS_COLORS="di=34"
 
 # user rc.
 if [ -e $ANTH_HOME/urc.sh ]; then
