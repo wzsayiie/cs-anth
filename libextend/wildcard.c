5bd2ad1e30d1460325e788d98b1d791e853c9595
吴佳 <wzsayiie@qq.com>
2022-10-22 17:53:06 +0800

    added file collecting by wildcards.

--- libextend/wildcard.c
+++ libextend/wildcard.c
@@ -1,4 +1,7 @@
 #include "wildcard.h"
+#include "fileio.h"
+#include "stdlib.h"
+#include "strex.h"
 
 bool iswcpat(const char *pat) {
     while (*pat) {
@@ -50,4 +53,74 @@ bool wcmatch(const char *pat, const char *str) {
     }
 
     return !*pat && !*str;
+}
+
+char **wccopyfiles(const char *pat, int *num) {
+    //the pattern does not contain wildcards.
+    if (!iswcpat(pat)) {
+        char **files = malloc(sizeof(char *));
+
+        *files = strdup(pat);
+        *num   = 1;
+
+        return files;
+    }
+
+    char *dir = strdup(pat);
+    dirname(dir);
+
+    //wildcards in directory path is unsupported.
+    if (iswcpat(dir)) {
+        free(dir);
+
+        *num = 0;
+        return NULL;
+    }
+
+    int    candnum = 0;
+    char **candids = dcopyitems(dir, &candnum);
+
+    if (candnum == 0) {
+        free(dir);
+
+        *num = 0;
+        return NULL;
+    }
+
+    //filter out suiable files:
+    char **files = NULL;
+    *num = 0;
+
+    size_t dlen = strlen(dir);
+    char  *base = strdup(pat);
+    basename(base);
+
+    for (int i = 0; i < candnum; ++i) {
+        char *cand = candids[i];
+        if (!wcmatch(base, cand)) {
+            continue;
+        }
+
+        size_t clen = strlen(cand);
+        char  *file = malloc(dlen + 1 + clen + 1);
+        strcpy (file, dir);
+        pathcat(file, candids[i]);
+
+        files = realloc(files, sizeof(char *) * (*num + 1));
+        files[*num] = file;
+        *num += 1;
+    }
+
+    dfreeitems(candids, candnum);
+    free(base);
+    free(dir);
+
+    return files;
+}
+
+void wcfreefiles(char **files, int num) {
+    for (int i = 0; i < num; ++i) {
+        free(files[i]);
+    }
+    free(files);
 }
--- libextend/wildcard.h
+++ libextend/wildcard.h
@@ -7,3 +7,6 @@
 
 __libf bool iswcpat(const char *pat);
 __libf bool wcmatch(const char *pat, const char *str);
+
+__libf char **wccopyfiles(const char *pat, int *num);
+__libf void   wcfreefiles(char    **files, int  num);
